ef6ed78963d5f908e3728095028d1393301fa195
Rudi van Hierden <rudi@draad.nl>
2023-01-09 13:36:25 +0100

    build: add logging to check variables

--- packages/cms/lib/modules/event-location-widgets/public/js/main.js
+++ packages/cms/lib/modules/event-location-widgets/public/js/main.js
@@ -2,6 +2,7 @@ apos.define('event-location-widgets', {
     extend: 'map-widgets',
     construct: function(self, options) {
         self.playAfterlibsLoaded = function($widget, data, options) {
+            console.log('<=== playAfterlibsLoaded event-location-widgets', $widget, data, options, data.activeResource, options.activeResource)
             var map = self.createMap(data.mapConfig);
             self.addPolygon(data.mapConfig);
             self.setIdeaMarker(data.mapConfig);
