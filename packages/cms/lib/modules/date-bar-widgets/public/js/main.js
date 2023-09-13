e79e911ab17c77f865930e19238a0dfb7a81d6d9
iandebruin98 <ian@draad.nl>
2023-09-13 09:38:03 +0200

    feat: Only show third number when needed

--- packages/cms/lib/modules/date-bar-widgets/public/js/main.js
+++ packages/cms/lib/modules/date-bar-widgets/public/js/main.js
@@ -25,7 +25,13 @@ $(document).ready(function () {
        const remaining = daysLeft1 % 100;
        value00 = parseInt(remaining / 10) || 0;
        value0 = remaining % 10;
-       $dateBar.find('.end-date-number-plate-000-1').html(value000);
+
+      if (value000 !== 0) {
+        $dateBar.find('.end-date-number-plate-000-1').html(value000);
+      } else {
+        $dateBar.find('.end-date-number-plate-000-1').hide();
+      }
+
        $dateBar.find('.end-date-number-plate-00-1').html(value00);
        $dateBar.find('.end-date-number-plate-0-1').html(value0);
       } else {
@@ -33,7 +39,7 @@ $(document).ready(function () {
 
        $dateBar.find('.end-date-bar-start-text-1').html( endDateText);
        $dateBar.find('.end-date-bar-end-text-1').html('dagen');
-       $dateBar.find('.end-date-number-plate-000-1').html(0);
+       $dateBar.find('.end-date-number-plate-000-1').html(0).hide();
        $dateBar.find('.end-date-number-plate-00-1').html(0);
        $dateBar.find('.end-date-number-plate-0-1').html(0);
       }
