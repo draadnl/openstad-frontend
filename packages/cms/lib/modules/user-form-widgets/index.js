8447b38e8ff4a8de13e0e8b4fb3f229beb6235dd
iandebruin98 <ian@draad.nl>
2023-09-28 14:52:51 +0200

    feat: Change ID generate function

--- packages/cms/lib/modules/user-form-widgets/index.js
+++ packages/cms/lib/modules/user-form-widgets/index.js
@@ -214,7 +214,7 @@ module.exports = {
         self.load = (req, widgets, callback) => {
             widgets.forEach((widget) => {
                 if (widget.containerStyles) {
-                    const containerId = styleSchema.generateId();
+                    const containerId = self.apos.utils.generateId();
                     widget.containerId = containerId;
                     widget.formattedContainerStyles = styleSchema.format(containerId, widget.containerStyles);
                     widget.mappedValidation = mapFormValidations(widget.formFields);
