3eb3bb17d6fa5f37a05dca1388b5932e3e4910d4
iandebruin98 <ian@draad.nl>
2024-05-29 16:58:21 +0200

    feat: Add option to show/hide modbreak

--- packages/cms/lib/modules/begroot-widgets/index.js
+++ packages/cms/lib/modules/begroot-widgets/index.js
@@ -58,6 +58,22 @@ const fields = [
     def: true
   },
   {
+    name: 'showModBreak',
+    label: 'Show Mod Break?',
+    type: 'boolean',
+    choices: [
+      {
+        label: 'Yes',
+        value: true,
+      },
+      {
+        label: 'No',
+        value: false,
+      }
+    ],
+    def: false
+  },
+  {
     name: 'votingType',
     type: 'select',
     label: 'Voting type',
